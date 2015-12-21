44ac596b5b60fb15f2fb79ef35950e6a48e76ff3
Moti Zilberman <motiz88@gmail.com>
2015-12-21 20:37:39 +0200

    Remove coffee-script runtime dependency, set up to publish only JS to npm

--- index.js
+++ index.js
@@ -2,11 +2,7 @@
 /* jshint -W097 */
 'use strict';
 
-
-var coffee = require('coffee-script');
-coffee.register();
-
 module.exports = {};
-module.exports.FtpConnectionError = require('./lib/ftpConnectionError');
-module.exports.FtpReconnectError = require('./lib/ftpReconnectError');
-module.exports.STATUSES = require('./lib/connectionStatuses');
+module.exports.FtpConnectionError = require('./dist/ftpConnectionError');
+module.exports.FtpReconnectError = require('./dist/ftpReconnectError');
+module.exports.STATUSES = require('./dist/connectionStatuses');
